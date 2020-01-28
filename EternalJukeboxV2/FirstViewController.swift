1421fb84228ac6a979f82eeccadb2e2bb47a790c
Bryce Harty <35123020+bhartyrs@users.noreply.github.com>
2020-01-28 12:53:34 -0500

    Initial Commit

---
+++ EternalJukeboxV2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EternalJukeboxV2
+//
+//  Created by Bryce Harty on 1/28/20.
+//  Copyright © 2020 OrangeTeam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
