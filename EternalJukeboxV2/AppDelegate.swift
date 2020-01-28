9c10b0e10818cb5b17eb690532c4cc0c1f6ae18f
Bryce Harty <35123020+bhartyrs@users.noreply.github.com>
2020-01-28 13:16:26 -0500

    SwiftUI->Storyboard
    This should fix everything as far as UI

--- EternalJukeboxV2/AppDelegate.swift
+++ EternalJukeboxV2/AppDelegate.swift
@@ -1,17 +1,22 @@
-//
+	//
 //  AppDelegate.swift
-//  EternalJukeboxV2
+//  Eternal Jukebox
 //
-//  Created by Bryce Harty on 1/28/20.
-//  Copyright © 2020 OrangeTeam. All rights reserved.
+//  Created by Capstone Project on 1/22/20.
+//  Copyright © 2020 Capstone Project. All rights reserved.
 //
 
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
 
-
+    /*I think these two variables do something.
+    I just added them because it muted some bugs in the code.
+    Not really sure if they help anything or not.
+    */
+    var playURI = ""
+    var accessToken = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +36,80 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+//Beginning of Added Code
+    
+    //Instantiating SPTConfiguration using my clientID and RedirectURL
+    let SpotifyClientID = "ece2e7fdbb0c43938876a3926c2143de"
+    let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+
+    lazy var configuration = SPTConfiguration(
+      clientID: SpotifyClientID,
+      redirectURL: SpotifyRedirectURL
+    )
+    
+    //Implementation of SPTAppRemoteDelegate and SPTAppRemotePlayerStateDelegate functions
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+      print("connected")
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+        })
+    }
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+      print("disconnected")
+    }
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+      print("failed")
+    }
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+      print("player state changed")
+        debugPrint("Track name: %@", playerState.track.name)
+    }
+    
+    //Authorization Callback
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      let parameters = appRemote.authorizationParameters(from: url);
+
+            if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+                appRemote.connectionParameters.accessToken = access_token
+                self.accessToken = access_token
+            } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+                // Show the error
+            }
+      return true
+    }
+    
+    //Initializing App Remote
+    lazy var appRemote: SPTAppRemote = {
+      let appRemote = SPTAppRemote(configuration: self.configuration, logLevel: .debug)
+      appRemote.connectionParameters.accessToken = self.accessToken
+      appRemote.delegate = self
+      return appRemote
+    }()
+    
+    //Authorizing and Connecting to Spotify
+    func connect() {
+      self.appRemote.authorizeAndPlayURI(self.playURI)
+    }
+    
+    //Disconnect from App Remote when application is switched
+    func applicationWillResignActive(_ application: UIApplication) {
+        if self.appRemote.isConnected {
+            self.appRemote.disconnect()
+        }
+    }
+    
+    //Reconnects to App Remote when application is re-opened
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            self.appRemote.connect()
+        }
+    }
+    
+ //Ending of added code
 
 
 }
