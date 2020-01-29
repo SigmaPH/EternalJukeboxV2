81c456aadf5fbd5c5964f92de1917284111af7e4
cnl4e <42973677+cnl4e@users.noreply.github.com>
2020-01-29 16:18:38 -0500

    Spotify Demos to Our Project
    Following the development path of using the SwiftUI demos from Spotify iOS SDK to make a Storyboard application.

--- EternalJukeboxV2/FirstViewController.swift
+++ EternalJukeboxV2/FirstViewController.swift
@@ -8,15 +8,94 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
+    
+    fileprivate let SpotifyClientID = "ece2e7fdbb0c43938876a3926c2143de"
+    fileprivate let SpotifyRedirectURI = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+    
+    lazy var configuration: SPTConfiguration = {
+        let configuration = SPTConfiguration(clientID: SpotifyClientID, redirectURL: SpotifyRedirectURI)
+        
+        configuration.playURI = ""
+        
+        configuration.tokenSwapURL = URL(string: "http://localhost:1234/swap")
+        configuration.tokenRefreshURL = URL(string: "http://localhost:1234/refresh")
+        
+        return configuration
+    }()
+    
+    lazy var sessionManager: SPTSessionManager = {
+        let manager = SPTSessionManager(configuration: configuration, delegate: self)
+        return manager
+    }()
 
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.delegate = self
+        return appRemote
+    }()
+
+    fileprivate var lastPlayerState: SPTAppRemotePlayerState?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func LoginButton(_ sender: Any) {
     }
     
-}
+    // MARK: - SPTSessionManagerDelegate
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        presentAlertController(title: "Authorization Failed", message: error.localizedDescription, buttonTitle: "Bummer")
+    }
 
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        presentAlertController(title: "Session Renewed", message: session.description, buttonTitle: "Sweet")
+    }
+
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        appRemote.connectionParameters.accessToken = session.accessToken
+        appRemote.connect()
+    }
+    
+    // MARK: -SPTAppRemoteDelegate
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        updateViewBasedOnConnected()
+        appRemote.playerAPI?.delegate = self
+        appRemote.playerAPI?.subscribe(toPlayerState: { (success, error) in
+            if let error = error {
+                print("Error subscribing to player state:" + error.localizedDescription)
+            }
+        })
+        fetchPlayerState()
+    }
+
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        updateViewBasedOnConnected()
+        lastPlayerState = nil
+    }
+
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        updateViewBasedOnConnected()
+        lastPlayerState = nil
+    }
+    
+    //MARK: - SPTAppRemotePlayerAPIDelegate
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        update(playerState: playerState)
+    }
+    
+    //MARK: -Private Helpers
+    
+    fileprivate func presentAlertController(title: String, message: String, buttonTitle: String) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        controller.addAction(action)
+        present(controller, animated: true)
+    }
+
+}
